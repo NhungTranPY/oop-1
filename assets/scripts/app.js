f930481c7e6bfd18f5800b276b5507e135aeede2
NhungTranPY <nhungtranpy91@gmail.com>
2023-01-12 21:42:56 +0700

    243. Working with Constructor Methods

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,24 +3,30 @@ class Product {
     imageUrl;
     description;
     price;
-}
 
-console.log(new Product());
+    constructor(title, image, desc, price) {
+        this.title = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.price = price;
+    }
+}
+// console.log(new Product());
 
 const productList = {
     products: [
-        {
-            title: 'A Pillow',
-            imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.slKai5OCE06BzTKxg6nUtgHaHa%26pid%3DApi&f=1&ipt=c1a29749fc340860cb3ee6bef75242fef169ebf9e4d1240addf22a851edf8028&ipo=images',
-            price: 19.19,
-            description: 'A soft pillow'
-        },
-        {
-            title: 'A Carpet',
-            imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.ZobBsKpwKY7dZVblJqUBjQHaFj%26pid%3DApi&f=1&ipt=44fa6eb3cdaac4b50f7ba5445d013aa8075f885b6f454bf0590f03845c227add&ipo=images',
-            price: 89.99,
-            description: 'A carpet which you might like - or not.'
-        }
+        new Product(
+            'A Pillow',
+            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.slKai5OCE06BzTKxg6nUtgHaHa%26pid%3DApi&f=1&ipt=c1a29749fc340860cb3ee6bef75242fef169ebf9e4d1240addf22a851edf8028&ipo=images',
+            'A soft pillow',
+            19.19
+        ),
+        new Product(
+            'A Carpet',
+            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.ZobBsKpwKY7dZVblJqUBjQHaFj%26pid%3DApi&f=1&ipt=44fa6eb3cdaac4b50f7ba5445d013aa8075f885b6f454bf0590f03845c227add&ipo=images',
+            'A carpet which you might like - or not.',
+            89.99
+        )
     ],
     render() {
         const renderHook = document.getElementById('app')
