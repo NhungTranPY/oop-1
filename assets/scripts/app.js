a8ca60960b2fa0dfaac2cce0dc9963321394831a
NhungTranPY <nhungtranpy91@gmail.com>
2023-01-12 21:48:47 +0700

    244. Fields vs Properties

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,9 @@
 class Product {
-    title = 'DEFAULT';
-    imageUrl;
-    description;
-    price;
-
+    // title = 'DEFAULT';
+    // imageUrl;
+    // description;
+    // price;
+    // // inside constructor is 'class property', if we comment the above, it still works
     constructor(title, image, desc, price) {
         this.title = title;
         this.imageUrl = image;
