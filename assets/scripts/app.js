bb4ded777f63c42b24b51549651d24098e62bbff
NhungTranPY <nhungtranpy91@gmail.com>
2023-01-13 22:10:08 +0700

    249. Static Methods & Properties

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -16,6 +16,11 @@ class Product {
 class ShoppingCart {
     items = []
 
+    addProduct(product) {
+        this.items.push(product)
+        this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`
+    }
+
     render() {
         const cartEl = document.createElement('section')
         cartEl.innerHTML = `
@@ -23,6 +28,7 @@ class ShoppingCart {
             <button>Order Now!</button>
         `
         cartEl.className = 'cart'
+        this.totalOutput = cartEl.querySelector('h2')
         return cartEl
     }
 }
@@ -33,8 +39,9 @@ class ProductItem {
     }
 
     addToCart() {
-        console.log('Adding product to cart...');
-        console.log(this.product);
+        // console.log('Adding product to cart...');
+        // console.log(this.product);
+        App.addProductToCart(this.product)
     }
 
     render() {
@@ -90,8 +97,8 @@ class ProductList {
 class Shop {
     render() {
         const renderHook = document.getElementById('app')
-        const cart = new ShoppingCart()
-        const cartEl = cart.render()
+        this.cart = new ShoppingCart()
+        const cartEl = this.cart.render()
         const productList = new ProductList()
         const prodListEl = productList.render()
 
@@ -100,6 +107,18 @@ class Shop {
     }
 }
 
-const shop = new Shop()
-shop.render()
+class App {
+    static cart;
+    
+    static init() {
+        const shop = new Shop()
+        shop.render()
+        this.cart = shop.cart
+    }
 
+    static addProductToCart(product) {
+        this.cart.addProduct(product)
+    }
+}
+
+App.init()
